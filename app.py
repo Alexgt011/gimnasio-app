31066b2aef07cf566c785157beaced348af4d4f6
Alexander <alexgutierrezt69@gmail.com>
2025-06-02 16:50:11 -0400

    Cambios para versión 3.0

--- app.py
+++ app.py
@@ -169,3 +169,4 @@ def updatepromociones(id):
 if __name__ == '__main__':
     app.run(debug=True)"// Cambios para la v2.0" 
 "// Fix aplicado para versi¢n 2.1" 
+"// Funcionalidades para versi¢n 3.0" 
